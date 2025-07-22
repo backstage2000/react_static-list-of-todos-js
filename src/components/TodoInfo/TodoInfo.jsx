c0b1c39c7ab41f18be021fc889a5c4789900c4f9
Anatoliy Korotenko <tolik.korotenko@21gmail.com>
2025-07-22 13:22:11 +0300

    Message

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,22 @@
 // Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+import cn from 'classnames';
+import './TodoInfo.scss';
+
+export const TodoInfo = ({ todos }) => {
+  return (
+    <article
+      className={cn('TodoInfo', {
+        'TodoInfo--completed': todos.completed,
+      })}
+    >
+      <h2 className="TodoInfo__title">{todos.title}</h2>
+      {todos.user && <UserInfo user={todos.user} />}
+    </article>
+  );
+};
+
+const UserInfo = ({ user }) => (
+  <a className="UserInfo" href={`mailto:${user.email}`}>
+    {user.name}
+  </a>
+);
